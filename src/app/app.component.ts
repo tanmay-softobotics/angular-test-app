046a4a3835c2afb20d1cf3c942dfb2286c40adfc
ghassen.ben <ghassen@encore-lab.com>
2023-02-14 12:24:38 +0100

    Creation of pagination logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,39 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Movie } from './application/models/Movie';
 import { MoviesApiService } from './services/movies-api.service';
+import { PaginationService } from './services/pagination.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent implements OnInit, OnDestroy {
   public title = 'skeleton-ts-angular';
-  private subscription: Subscription;
-  public movies : Movie[] = [];
+  private moviesSubscription: Subscription;
+  private pageSubscription: Subscription;
+  public movies: Movie[] = [];
 
-  constructor(private moviesApi: MoviesApiService) {
-    this.subscription = new Subscription();
+  constructor(private moviesApiService: MoviesApiService, private paginationService : PaginationService) {
+    this.moviesSubscription = new Subscription();
+    this.pageSubscription = new Subscription();
   }
 
   ngOnInit(): void {
-    this.subscription = this.moviesApi.getMovies().subscribe({
+    this.moviesSubscription = this.moviesApiService.getMovies().subscribe({
       next: (response) => {
-        this.movies = response.entries;
+        this.pageSubscription = this.paginationService.currentPage$.subscribe(() => {
+          this.movies = this.paginationService.getPagedData(response);          
+        });
       },
       error: (error) => console.error(error),
     });
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.moviesSubscription.unsubscribe();
+    this.pageSubscription.unsubscribe();
   }
 }
 
--- src/app/services/pagination.service.ts
+++ src/app/services/pagination.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ResponseApi } from '../application/models/ResponseApi';
+import { BehaviorSubject } from 'rxjs';
+import { Movie } from '../application/models/Movie';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaginationService {
+  private currentPageSubject = new BehaviorSubject<number>(1);
+  currentPage$ = this.currentPageSubject.asObservable();
+  private moviesPerPage = 10;
 
-  constructor() { }
+  constructor() {}
+
+  setCurrentPage(page: number) {
+    this.currentPageSubject.next(page);
+  }
+
+  getPagedData(responseApi: ResponseApi): Movie[] {
+    const startIndex = (this.currentPageSubject.value - 1) * this.moviesPerPage;
+    const endIndex = startIndex + this.moviesPerPage;
+    return responseApi.entries.slice(startIndex, endIndex);
+  }
+
 }
