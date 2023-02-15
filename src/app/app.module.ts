8bb852955aacb1a3d9877037a2672312a8badd3d
ghassen.ben <ghassen@encore-lab.com>
2023-02-15 20:56:35 +0100

    Adding tests for app, movies, pagination and search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 import { SearchBarComponent } from './ui/search-bar/search-bar.component';
--- src/app/ui/search-bar/search-bar.component.spec.ts
+++ src/app/ui/search-bar/search-bar.component.spec.ts
@@ -1,17 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { SearchBarComponent } from './search-bar.component';
+import { MoviesApiService } from 'src/app/services/movies-api.service';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  let moviesApiService: jasmine.SpyObj<MoviesApiService>;
 
   beforeEach(async () => {
+    const moviesApiServiceSpy = jasmine.createSpyObj('MoviesApiService', ['search']);
+
     await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      declarations: [ SearchBarComponent ],
+      providers: [
+        { provide: MoviesApiService, useValue: moviesApiServiceSpy },
+      ]
     })
     .compileComponents();
 
+    moviesApiService = TestBed.inject(MoviesApiService) as jasmine.SpyObj<MoviesApiService>;
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +30,13 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call moviesApiService.search with input string', () => {
+    const input = 'test input';
+    const inputElement = fixture.debugElement.query(By.css('input'));
+    inputElement.nativeElement.value = input;
+    inputElement.triggerEventHandler('input', { target: inputElement.nativeElement });
+    expect(moviesApiService.search).toHaveBeenCalledWith(input);
   });
 });
